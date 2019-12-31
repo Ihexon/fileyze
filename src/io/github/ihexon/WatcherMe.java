8310fa0c7d03ab51aae641ceafc7293924133e4a
FlyingMcDonald <1476211809@qq.com>
2019-12-31 22:52:38 +0800

    add some features

--- src/io/github/ihexon/WatcherMe.java
+++ src/io/github/ihexon/WatcherMe.java
@@ -4,6 +4,8 @@ import static java.nio.file.StandardWatchEventKinds.*;
 import static java.nio.file.LinkOption.*;
 import java.nio.file.attribute.*;
 import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class WatcherMe {
@@ -11,6 +13,10 @@ public class WatcherMe {
     private final WatchService watcher;
     private final Map<WatchKey,Path> keys;
     private final boolean recursive;
+    private PrintStream out;
+    private PrintStream outFile;
+    private List<String> specialFile;
+    private SimpleDateFormat currentDate;
     private boolean trace = false;
 
     @SuppressWarnings("unchecked")
@@ -22,18 +28,25 @@ public class WatcherMe {
      * Register the given directory with the WatchService
      */
     private void register(Path dir) throws IOException {
-        WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
-        if (trace) {
-            Path prev = keys.get(key);
-            if (prev == null) {
-                System.out.format("register: %s\n", dir);
-            } else {
-                if (!dir.equals(prev)) {
-                    System.out.format("update: %s -> %s\n", prev, dir);
+        String dirStr = dir.toString();
+        //不为特殊文件的话，将该文件注册监听，否则不做处理
+        if(!specialFile.contains(dirStr)){
+            WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+            if (trace) {
+                Path prev = keys.get(key);
+                if (prev == null) {
+                    System.out.format("register: %s\n", dir);
+                } else {
+                    if (!dir.equals(prev)) {
+                        System.out.format("update: %s -> %s\n", prev, dir);
+                    }
                 }
             }
+            keys.put(key, dir);
+        }else{
+            System.out.println("Warning：This dir contains special dir："+dirStr+"\n"+"Special dir will skip");
         }
-        keys.put(key, dir);
+
     }
 
     /**
@@ -42,15 +55,20 @@ public class WatcherMe {
      */
     private void registerAll(final Path start) throws IOException {
         // register directory and sub-directories
-        Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
-            @Override
-            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
-                    throws IOException
-            {
-                register(dir);
-                return FileVisitResult.CONTINUE;
-            }
-        });
+        try {
+            Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
+                        throws IOException
+                {
+                    register(dir);
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        }catch (AccessDeniedException e){
+            System.err.println("Error！ Existing file or folder permissions are insufficient to monitor："+e.getMessage());
+            System.exit(-1);
+        }
     }
 
     /**
@@ -60,7 +78,10 @@ public class WatcherMe {
         this.watcher = FileSystems.getDefault().newWatchService();
         this.keys = new HashMap<WatchKey,Path>();
         this.recursive = recursive;
-
+        this.specialFile = new ArrayList<String>(Arrays.asList("", "This list for Special file "));
+        this.out = System.out;
+        this.outFile = new PrintStream("log.txt");
+        this.currentDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         if (recursive) {
             System.out.format("Scanning %s ...\n", dir);
             registerAll(dir);
@@ -77,7 +98,8 @@ public class WatcherMe {
      * Process all events for keys queued to the watcher
      */
     void processEvents() {
-        for (;;) {
+        System.setOut(outFile);
+        while(true) {
 
             // wait for key to be signalled
             WatchKey key;
@@ -92,10 +114,10 @@ public class WatcherMe {
                 System.err.println("WatchKey not recognized!!");
                 continue;
             }
+            List<WatchEvent<?>> eventList = key.pollEvents();
 
-            for (WatchEvent<?> event: key.pollEvents()) {
-                WatchEvent.Kind kind = event.kind();
-
+            for (WatchEvent<?> event: eventList) {
+                WatchEvent.Kind<?> kind = event.kind();
                 // TBD - provide example of how OVERFLOW event is handled
                 if (kind == OVERFLOW) {
                     continue;
@@ -107,8 +129,10 @@ public class WatcherMe {
                 Path name = ev.context();
                 Path child = dir.resolve(name);
 
-                // print out event
-                System.out.format("%s: %s\n", event.kind().name(), child);
+                // print out event to log file
+
+                System.out.format("%s: %s: %s\n", currentDate.format(new Date()), event.kind().name(), child);
+
 
                 // if directory is created, and watching recursively, then
                 // register it and its sub-directories
@@ -134,6 +158,7 @@ public class WatcherMe {
                 }
             }
         }
+        System.setOut(out);
     }
 
     static void usage() {
@@ -143,19 +168,20 @@ public class WatcherMe {
 
     public static void main(String[] args) throws IOException {
         // parse arguments
-        if (args.length == 0 || args.length > 2)
-            usage();
-        boolean recursive = false;
-        int dirArg = 0;
-        if (args[0].equals("-r")) {
-            if (args.length < 2)
-                usage();
-            recursive = true;
-            dirArg++;
-        }
+//        if (args.length == 0 || args.length > 2)
+//            usage();
+        boolean recursive = true;
+//        int dirArg = 0;
+//        if (args[0].equals("-r")) {
+//            if (args.length < 2)
+//                usage();
+//            recursive = true;
+//            dirArg++;
+//        }
+//        args[dirArg]
 
         // register directory and process its events
-        Path dir = Paths.get(args[dirArg]);
+        Path dir = Paths.get("/home/yangyunfei/Desktop");
         new WatcherMe(dir, recursive).processEvents();
     }
 }
